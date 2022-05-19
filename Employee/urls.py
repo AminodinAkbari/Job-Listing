9853aef4e3c92361e865977e6770ab5ff1da653d
Amin Akbari <13amin80badboy@gmail.com>
2022-05-19 23:24:27 +0430

    add all urls for employee activities

--- Employee/urls.py
+++ Employee/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
-from .views import test_seassion
+from django.contrib.auth.decorators import login_required
+
+from Accounts.views import EmployeeRegisterView
+from Employee.views import *
 urlpatterns = [
-path('test_seassion',test_seassion)
+	path('resume/<int:pk>' , login_required(UpdateResume.as_view()) , name = 'UpdateResume'),
+	path('jobs-apply/<int:pk>' , login_required(EmployeeJobApply) , name = 'EmployeeJobApply'),
+	path('apply/<int:ad>' , login_required(ApplicantView) , name = 'ApplicantView'),
+	path('add_favorite/<int:ad>' , login_required(FavoriteView) , name = 'Favorite'),
+	path('ads-saved/<int:pk>' , login_required(AdSaved) , name = 'AdsSaved'),
+	path('ad-delete/<int:pk>' , login_required(AdUnsaved) , name = 'AdUnsaved'),
 ]
