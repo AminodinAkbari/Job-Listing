015eaaf34b1404a634a6fa85dbda25b007e01bbc
Amin Akbari <13amin80badboy@gmail.com>
2022-05-19 23:10:39 +0430

    add employee_owner_can_access and file_size defines

--- Controllers/views.py
+++ Controllers/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
 from Employer.models import Manager
+from Employee.models import EmployeeModel
 # Create your views here.
 def Who_is(func):
 	def wrapper(request,*args , **kwargs):
@@ -10,4 +11,24 @@ def Who_is(func):
 		return func(request,*args , **kwargs , user_type = manager)
 	return wrapper
 
+
+def employee_owner_can_access(func):
+	def wrapper(request ,pk , *args , **kwargs):
+		try:
+			employee = EmployeeModel.objects.filter(employee = pk).first()
+		except:
+			employee = None
+			return redirect('/')
+
+		if employee is not None:
+			if employee.employee != request.user:
+				return redirect('/')
+			
+		return func(request , pk ,*args , **kwargs , employee = employee)
+	return wrapper
+
+def file_size(value): # add this to some file where you can import it from
+	limit = 2 * 1024 * 1024
+	if value.size > limit:
+		raise ValidationError('حجم تصویر زیاد است ! اندزه فایل نباید بالای 2مگابایت باشد')
 
