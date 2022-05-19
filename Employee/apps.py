dff8ad4bb0d62a40dac051130198f5941f638fad
Amin Akbari <13amin80badboy@gmail.com>
2022-05-19 23:21:42 +0430

    NewEmployee AppConfig For employee signal in signals.py

--- Employee/apps.py
+++ Employee/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
 
-
 class EmployeeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Employee'
+ 
+class NewEmployee(AppConfig):
+    name = 'Employee'
+    def ready(self):
+        import Employee.signals
