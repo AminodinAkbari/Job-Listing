980c72f45286ef06133de08d53ef4a4dcf0a4965
Amin Akbari <13amin80badboy@gmail.com>
2022-05-21 22:09:44 +0430

    new pathes for new employer/views

--- Employer/urls.py
+++ Employer/urls.py
@@ -4,8 +4,21 @@ from django.contrib.auth.decorators import login_required
 from .views import (
 	NewAd,
 	NewCompany,
+	ManagerPanel,
+	DeleteAd,
+	EditCompanyView,
+	DeleteCompany,
+	EditAdView,
 )
+from .views import 	EditMangerInfo , UpdatePasswordManager
 urlpatterns = [
 	path('new_ad' , login_required(NewAd) , name = 'NewAd'),
 	path('new_company' , login_required(NewCompany) , name = 'NewCompany'),
+	path('edit_manager_info/<int:pk>' , login_required(EditMangerInfo.as_view()) , name = 'EditManagerInfo'),
+	path('edit_company_info/<int:pk>' , login_required(EditCompanyView.as_view()) , name = 'EditCompanyView'),
+	path('update_manager_password' , login_required(UpdatePasswordManager) , name = 'UpdatePasswordManager'),
+	path('manager_panel/<int:pk>' , login_required(ManagerPanel.as_view()) , name = 'ManagerPanel'),
+	path('editad_view/<int:pk>' , login_required(EditAdView.as_view()) , name = 'EditAdView'),
+	path('delete_ad/<int:pk>' , login_required(DeleteAd) , name = 'DeleteAd'),
+	path('delete_company/<int:pk>' , login_required(DeleteCompany) , name = 'DeleteCompany'),
 ]
