0eafd56b0763ee9ace3f0e8e1bd70ecebc45b6ef
Amin Akbari <13amin80badboy@gmail.com>
2022-05-21 22:05:02 +0430

    add aplicant admin registers

--- Employer/admin.py
+++ Employer/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Manager , Company , Advertisement
+from .models import Manager , Company , Advertisement,Applicant
 
 from jalali_date import datetime2jalali, date2jalali
 from jalali_date.admin import ModelAdminJalaliMixin
@@ -14,6 +14,9 @@ class CompanyAdmin(admin.ModelAdmin):
 
 class AdAdmin(admin.ModelAdmin):
 	list_display = ['__str__' , 'company' , 'category' , 'salary' , 'expired' ,'generate_in']
+
+class ApplicantAdmin(admin.ModelAdmin):
+	list_display = ['user' , 'ad' , 'created_at']
 	
 	def get_created_jalali(self, obj):
 		return datetime2jalali(obj.created).strftime('%y/%m/%d _ %H:%M:%S')
@@ -21,3 +24,4 @@ class AdAdmin(admin.ModelAdmin):
 admin.site.register(Manager,ManagerAdmin)
 admin.site.register(Company,CompanyAdmin)
 admin.site.register(Advertisement,AdAdmin)
+admin.site.register(Applicant,ApplicantAdmin)
