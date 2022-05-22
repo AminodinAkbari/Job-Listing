66a72621ff3c1328be8fc2cecda6f3806d09a492
Amin Akbari <13amin80badboy@gmail.com>
2022-05-22 18:54:10 +0430

    add applicant detail and canceling views

--- Employee/views.py
+++ Employee/views.py
@@ -5,14 +5,17 @@ from django.shortcuts import get_object_or_404
 from .forms import PersonalInfo_ResumeForm
 from Controllers.views import Who_is ,employee_owner_can_access
 
-from django.views.generic.edit import FormView
-from django.views.generic.edit import UpdateView
+from django.views.generic.edit import FormView , UpdateView
+from django.views.generic import DetailView
 
 from Employer.models import Manager , Applicant , Advertisement
 from .models import EmployeeModel , Favorite
 from django.contrib.auth.models import User
 
 from django.contrib import messages
+
+from django.utils import timezone
+now = timezone.now()
 # Create your views here.
 
 class UpdateResume(UpdateView):
@@ -45,6 +48,23 @@ class UpdateResume(UpdateView):
 		messages.success(self.request , 'تغییرات با موفقیت ذخیره شد')
 		return reverse_lazy('UpdateResume' , kwargs = {'pk':obj.id})
 
+class ApplicantDetail(DetailView):
+	model = Applicant
+	template_name = 'Employee/Applicant-detail.html'
+
+	def dispatch(self ,request , *args, **kwargs):
+		obj = self.get_object()
+		if obj.user.username != request.user.username:
+			return redirect('/')
+		return super(ApplicantDetail , self).dispatch(request , *args , **kwargs)
+
+	def get_context_data(self , **kwargs):
+		context = super().get_context_data(**kwargs)
+		context['employee'] = EmployeeModel.objects.filter(employee = self.request.user).first()
+		obj = self.get_object()
+		context['time_left'] = obj.ad.expired_in.day-now.day
+		return context
+
 @Who_is
 def ApplicantView(request , ad , user_type):
 	if user_type is not None:
@@ -55,6 +75,18 @@ def ApplicantView(request , ad , user_type):
 	except:
 		Applicant.objects.create(user = request.user , ad = advertisement)
 	return redirect('/')
+
+@Who_is
+def canceling_applicant(request , pk , user_type):
+	if user_type is None:
+		try:
+			target = Applicant.objects.get(id = pk)
+			if target.user == request.user:
+				target.delete()
+		except:
+			return redirect('/')
+	return redirect('/')
+
 
 @Who_is
 def FavoriteView(request , ad , user_type):
@@ -73,6 +105,7 @@ def EmployeeJobApply(request , pk , employee):
 	applicants = Applicant.objects.filter(user = request.user)
 	context['employee'] = employee
 	context['Applicants'] = applicants
+	context['title'] = 'آگهی های ارسالی'
 	return render(request , 'Employee/employee-JobApply.html' , context)
 
 @employee_owner_can_access
