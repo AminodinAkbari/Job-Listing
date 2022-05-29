90714967c1dea6569a0d5554ae4f42c5585ef0d2
AminodinAkbari <13amin80badboy@gmail.com>
2022-05-29 23:25:51 +0430

    split to comppnents.py

--- Controllers/templatetags/Ads.py
+++ Controllers/templatetags/Ads.py
@@ -10,20 +10,15 @@ now = timezone.now()
 
 register = template.Library()
 
-@register.inclusion_tag('Employer/Advertising.html')
-def home_ads(request):
-	context = {}
-	qs = Advertisement.objects.filter(company__valid = True, expired_in__gt = now).order_by('-generate_in')[:12]
-	if request.user.is_authenticated:
-		applied_jobs = Applicant.objects.filter(user = request.user)
-		marked = Favorite.objects.filter(user = request.user)
-		str_list_for_template = []
-		for ad in applied_jobs:
-			str_list_for_template.append(ad.ad)
-		context['applied_jobs'] = str_list_for_template
-		context['marked'] = marked
-	context['Ads']=qs
-	return context
+# @register.inclusion_tag('Employer/Advertising.html')
+# def home_ads(request):
+# 	context = {}
+# 	qs = Advertisement.objects.filter(company__valid = True, expired_in__gt = now).order_by('-generate_in')[:12]
+# 	if request.user.is_authenticated:
+# 		context['applied_jobs'] = str_list_for_applid
+# 		context['marked'] = marked_list
+# 	context['Ads']=qs
+# 	return context
 
 @register.inclusion_tag('Employer/Advertising.html')
 def ads_job_apply(request):
