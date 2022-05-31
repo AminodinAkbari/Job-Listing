fb0d115eb105ce6d4a0812f4dc9fca7a830cb9a1
AminodinAkbari <13amin80badboy@gmail.com>
2022-05-31 23:53:31 +0430

    now can change employee name email password

--- Employee/views.py
+++ Employee/views.py
@@ -1,8 +1,9 @@
+from django.contrib.auth import authenticate
 from django.shortcuts import render , redirect
 from django.urls import reverse,reverse_lazy
 from django.shortcuts import get_object_or_404
 
-from .forms import PersonalInfo_ResumeForm
+from .forms import EditNameOrEmailForm, PersonalInfo_ResumeForm , ChangePassword_Employee
 from Controllers.views import Who_is ,employee_owner_can_access
 
 from django.views.generic.edit import FormView , UpdateView
@@ -14,6 +15,7 @@ from django.contrib.auth.models import User
 
 from django.contrib import messages
 
+from Controllers.models import passGenerator
 # from django.utils import timezone
 # now = timezone.now()
 # Create your views here.
@@ -43,6 +45,48 @@ class UpdateResume(UpdateView):
 		obj = self.get_object()
 		messages.success(self.request , 'تغییرات با موفقیت ذخیره شد')
 		return reverse_lazy('UpdateResume' , kwargs = {'pk':obj.id})
+
+
+def EditNameOrEmail_Employee(request , pk):
+	current_user = User.objects.filter(id = pk)
+	single_parametr = current_user.first()
+	context = {}
+	if request.method == "POST" and 'email-name' in request.POST:
+		form = EditNameOrEmailForm(request.POST)
+		if form.is_valid():
+			f_name = form.cleaned_data.get('first_name')
+			l_name = form.cleaned_data.get('last_name')
+			u_name = form.cleaned_data.get('username')
+			current_user.update(first_name = f_name , last_name = l_name , username = u_name )
+			messages.success(request , 'پروفایل شما بروزرسانی شد')
+			return redirect(reverse('EditNameOrEmail-Employee' , kwargs={'pk' : single_parametr.id}))
+	else:
+		form = EditNameOrEmailForm(initial = {'first_name':single_parametr.first_name ,
+		 'last_name':single_parametr.last_name , 'username':single_parametr.username})
+	if request.method == "POST" and 'change-password' in request.POST:
+		change_pass_form = ChangePassword_Employee(request.POST)
+		if change_pass_form.is_valid():
+			old = change_pass_form.cleaned_data.get('old')
+			new = change_pass_form.cleaned_data.get('new')
+			re_new = change_pass_form.cleaned_data.get('re_new')
+			user = authenticate(request, username = request.user.username ,password = old)
+			if user is not None:
+				if old == new:
+					change_pass_form.add_error('old' ,'رمز عبور جدید باید با رمز عبور حال حاضر تفاوت داشته باشد')
+				else:
+					single_parametr.set_password(new)
+					single_parametr.save()
+					messages.success(request , 'رمز عبور شما به روز رسانی شد . لطفا دوباره وارد شوید')
+					return redirect(reverse('Login'))
+			else:
+				change_pass_form.add_error('old' , 'رمز عبور وارد شده اشتباه است')
+	else:
+		change_pass_form = ChangePassword_Employee()
+	context['password_form'] = change_pass_form
+	context['form'] = form
+	context['title'] = 'تنظیمات حساب کاربری'
+	context['suggest_password'] = passGenerator(10)
+	return render(request , 'Employee/EditNameOrEmail-Employee.html' , context)
 
 import datetime
 today = datetime.datetime.now().date()
