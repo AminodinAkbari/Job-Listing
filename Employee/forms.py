a25839dfe5d51f4f60a30129c64f0a38d1e81ec2
AminodinAkbari <13amin80badboy@gmail.com>
2022-05-31 23:49:11 +0430

    add change name and email and password form

--- Employee/forms.py
+++ Employee/forms.py
@@ -7,9 +7,6 @@ from Controllers.views import file_size
 from jalali_date.fields import JalaliDateField
 from jalali_date.widgets import AdminJalaliDateWidget
 from django.contrib.auth.models import User
-from django.core.exceptions import ValidationError
-
-
 
 from Controllers.models import *
 
@@ -31,6 +28,7 @@ class PersonalInfo_ResumeForm(ModelForm):
             widget=AdminJalaliDateWidget
         )
 
+
 		for field in self.fields.values():
 			field.widget.attrs.update({'class': 'form-control rtl'})
 
@@ -42,3 +40,37 @@ class PersonalInfo_ResumeForm(ModelForm):
 		self.fields['languages'].label='به کدام زبان گفتاری مسلط هستید ؟'
 		self.fields['skills'].label='مهارت های خود را بنویسید (آنها را با علامت "/" جدا کنید)'
 		self.fields['birth'] = JalaliDateField(label=('تاریخ تولد'),widget=AdminJalaliDateWidget)
+
+class EditNameOrEmailForm(forms.Form):
+	first_name = forms.CharField(widget = forms.TextInput , label = 'نام')
+	last_name = forms.CharField(widget = forms.TextInput , label = 'نام خانوادگی')
+	username = forms.CharField(widget = forms.TextInput , label = 'ایمیل')
+	def __init__(self , *args , **kwargs):
+		super(EditNameOrEmailForm, self).__init__(*args, **kwargs)
+		for field in self.fields.values():
+			field.widget.attrs.update({'class': 'form-control rtl mb-3'})
+
+
+class ChangePassword_Employee(forms.Form):
+
+	def __init__(self , *args , **kwargs):
+		super(ChangePassword_Employee, self).__init__(*args, **kwargs)
+		for field in self.fields.values():
+			field.widget.attrs.update({'class': 'form-control rtl mb-2'})
+
+	old = forms.CharField(widget=forms.PasswordInput,label='رمز عبور حال حاضر')
+	new = forms.CharField(widget=forms.PasswordInput,label='رمز جدید')
+	re_new = forms.CharField(widget=forms.PasswordInput,label='تکرار رمز جدید')
+
+	def clean_new(self):
+		new = self.cleaned_data.get('new')
+		if len(new)<8:
+			raise forms.ValidationError('کلمه عبور باید حداقل 8 کاراکتر باشد')
+		return new
+
+	def clean_re_new(self):
+		new = self.cleaned_data.get('new')
+		re_new = self.cleaned_data.get('re_new')
+		if new != re_new :
+			raise forms.ValidationError('کلمه های عبور با یکدیگر مغایرت دارند !')
+		return re_new
