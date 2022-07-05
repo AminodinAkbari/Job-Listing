a2971094b5fbc89c42dd2cf4bf0696429e67fb3a
AminodinAkbari <13amin80badboy@gmail.com>
2022-07-05 21:22:21 +0430

    CBV URLS

--- Employee/urls.py
+++ Employee/urls.py
@@ -5,7 +5,7 @@ from Accounts.views import EmployeeRegisterView
 from Employee.views import *
 urlpatterns = [
 	path('resume/<int:pk>' , login_required(UpdateResume.as_view()) , name = 'UpdateResume'),
-	path('edit-name-or-email/<int:pk>' , login_required(EditNameOrEmail_Employee) , name = 'EditNameOrEmail-Employee'),
+	path('edit-name-or-email/<int:pk>' , login_required(EditNameOrEmail_Employee.as_view()) , name = 'EditNameOrEmail-Employee'),
 	path('jobs-apply' , login_required(EmployeeJobApply.as_view()) , name = 'EmployeeJobApply'),
 	path('apply/<int:ad>' , login_required(ApplicantView) , name = 'ApplicantView'),
 	path('applicant-detail/<int:pk>' , login_required(ApplicantDetail.as_view()) , name = 'ApplicantDetail'),
