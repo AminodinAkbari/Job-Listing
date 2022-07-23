1a9bd01def956982780791d6e073694cb678a8d0
AminodinAkbari <13amin80badboy@gmail.com>
2022-07-23 23:50:06 +0430

    improved and new apis

--- Rest_API/serializers.py
+++ Rest_API/serializers.py
@@ -1,5 +1,22 @@
 from rest_framework import serializers
 from Employer import models
+from Employee import models as EMP
+from django.contrib.auth.models import User
+
+# {
+# "username" : "amin",
+# "password" : "aminamin2018"
+# }
+
+class UserFullSerilizer(serializers.ModelSerializer):
+	class Meta:
+		model = User
+		fields = ['username' , 'password' , 'first_name' , 'last_name']
+
+class FavoriteModelSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = EMP.Favorite
+		fields = '__all__'
 
 class AllManagerFullSerializer(serializers.ModelSerializer):
 	class Meta:
@@ -19,4 +36,20 @@ class AdvertisementsFullSerializer(serializers.ModelSerializer):
 class AdvertisementMiniSerilalzer(serializers.ModelSerializer):
 	class Meta:
 		model = models.Advertisement
-		fields = ['title' , 'expired_in' , 'category']
+		fields = ['id','title' , 'expired_in' , 'category']
+
+class ApplicantFullSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = models.Applicant
+		fields = '__all__'
+
+class HrieModelFullSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = models.Hire
+		fields = '__all__'
+# END EMPLOYER serializers
+
+class EmployeeModelSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = EMP.EmployeeModel
+		exclude = ('profile_pic',)
