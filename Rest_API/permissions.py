e2bc50165f1ff9affb889cdd761045a31c9b2ceb
AminodinAkbari <13amin80badboy@gmail.com>
2022-07-23 23:53:30 +0430

    add two more permissions for employer and employee APIs

--- Rest_API/permissions.py
+++ Rest_API/permissions.py
@@ -1,12 +1,19 @@
 from rest_framework.permissions import BasePermission , SAFE_METHODS
+from Employee import models as emp_models
+from rest_framework.exceptions import PermissionDenied
+
 
 class IsAdManager(BasePermission):
 	def has_object_permission(self , request , view , obj):
-		return bool(
-			request.method in SAFE_METHODS or
-			request.user and
-			request.user.username == obj.company.manager.email
-		)
+		permission = bool(request.user and request.user.username == obj.company.manager.email)
+		if not permission :
+			raise PermissionDenied({"From Administrator" : "Only The Manager Of This AD Can Access To It !"})
+
+class OwnerCanSee(BasePermission):
+	def has_object_permission(self , request , view , obj):
+		permission = bool(request.user and obj.user.id == request.user.id)
+		if not permission :
+			raise PermissionDenied({"From Administrator" : "Only The Current Owner Of This Favorite Model Can Access !"})
 
 class IsSuperUser(BasePermission):
 	def has_permission(self , request , view):
