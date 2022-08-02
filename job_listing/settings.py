19d3e15583d5c66c3f6f81c136527d23c6ddc2b0
AminodinAkbari <13amin80badboy@gmail.com>
2022-08-02 20:21:59 +0430

    update static_root

--- job_listing/settings.py
+++ job_listing/settings.py
@@ -149,7 +149,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATICFILES_DIRS = [BASE_DIR/'UI-template']
-STATIC_ROOT = os.path.join(BASE_DIR / 'static')
+STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles')
 STATIC_URL = 'static/'
 MEDIA_ROOT =BASE_DIR / 'uploads'
 MEDIA_URL = '/media/'
